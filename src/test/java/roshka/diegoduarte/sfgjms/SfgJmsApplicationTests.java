a91277417f14c08d40c2afb2514e237db79c1a2e
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-05-08 10:44:12 -0300

    initial

---
+++ src/test/java/roshka/diegoduarte/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package roshka.diegoduarte.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

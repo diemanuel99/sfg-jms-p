47a5d814649848cb00e5efdd19df073838cfa2d5
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-05-09 16:56:11 -0300

    Send and receive

--- src/main/java/roshka/diegoduarte/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/roshka/diegoduarte/sfgjms/listener/HelloMessageListener.java
@@ -1,7 +1,10 @@
 package roshka.diegoduarte.sfgjms.listener;
 
+import jakarta.jms.JMSException;
 import jakarta.jms.Message;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,20 +12,39 @@ import org.springframework.stereotype.Component;
 import roshka.diegoduarte.sfgjms.config.JmsConfig;
 import roshka.diegoduarte.sfgjms.model.HelloWorldMessage;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers, Message message){
 
-        System.out.println("I Got a Message!!!!!");
+        //System.out.println("I Got a Message!!!!!");
 
-        System.out.println(helloWorldMessage);
+        // System.out.println(helloWorldMessage);
 
 
-        //uncomment and view to see retry count in debugger
-        throw new RuntimeException("foo");
+        // uncomment and view to see retry count in debugger
+        // throw new RuntimeException("foo");
+
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                               @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
 
     }
 }
